89c1c2fa26d7f8e04e4fb56d5291ca395ea2049d
juan_gomez9905 <juan.gomez@bebolder.co>
2024-09-06 19:10:33 -0500

    fixing 1,2,3 requeriments points

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Todo } from 'src/app/core/interfaces/todo.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('todoCreator') input!: ElementRef<HTMLInputElement>;
+  @ViewChild('editing') inputEditing!: ElementRef<HTMLInputElement>;
+
+  public newTodo = new FormControl();
+
+  public todos: Todo[] = [
+    {
+      id: '0',
+      title: 'comer',
+      completed: false,
+      isEditingMode: false
+    }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addNewTodo() {
+    if (!!this.newTodo.value) {
+      const newTodo: Todo = {
+        id: this.todos.length.toString(),
+        title: this.newTodo.value.trim(),
+        completed: false,
+        isEditingMode: false
+      };
+
+      this.todos.push(newTodo);
+      this.newTodo.setValue(null);
+      this.input.nativeElement.focus();
+    }
+  }
+
+  handleTodoState(event: Event, todoToChange: Todo) {
+    const inputEl = event.target as HTMLInputElement;
+    this.todos.forEach(todo => {
+        if (todo.id === todoToChange.id) {
+          todo.completed = inputEl.checked;
+        }
+      }
+    );
+  }
+
+  updateTitle(event: Event, todoToChange: Todo) {
+    const input = event.target as HTMLInputElement;
+    const value = input.value;
+
+    this.todos.forEach(todo => {
+      if (todo.id === todoToChange.id) {
+        todo.title = value;
+        todo.isEditingMode= false;
+      }
+    }
+  );
   }
 
 }
